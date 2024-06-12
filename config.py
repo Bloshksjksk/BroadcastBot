c17e72714c85e1ee260bb734a9c3b7a90c1a3137
Bloshksjksk <muscular.colossal.policeman@ghostlymail.com>
2024-06-12 08:33:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7294590146:AAHiQwqlNtqJPkHQ96j9d4KM_pCgAx6EH-o")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7294590146:AAEqmyr3O_6mU6IfT9h373YhHyf05-rmfmU")
 API_ID = int(os.environ.get("API_ID", "4682685"))
 API_HASH = os.environ.get("API_HASH", "3eba5d471162181b8a3f7f5c0a23c307")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001655909201"))
